5007792d9c8bc76a862cfe30b1e5a30c7e448931
Marina <marina.todorishina@gmail.com>
2018-03-16 17:18:15 +0200

    Add redux store to main App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import './client/styles/index.css';
 import App from './client/components/App';
 import registerServiceWorker from './client/components/registerServiceWorker';
+import reducer from './client/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
+
 registerServiceWorker();
