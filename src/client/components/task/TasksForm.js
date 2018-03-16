2af8474d6e988feaadf69577e4eeeb3a49fa77d7
Marina <marina.todorishina@gmail.com>
2018-03-17 00:01:11 +0200

    Add react-reduce-form methods, fix tasks saving

--- src/client/components/task/TasksForm.js
+++ src/client/components/task/TasksForm.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {LocalForm, Control, Errors} from 'react-redux-form';
+
 import tasks from './tasks';
 import Task from "./Task";
 
@@ -7,12 +9,13 @@ export default class TasksForm extends Component {
   constructor(props) {
     super(props);
 
+    this.tasksArr = tasks.allTasks;
+    console.log(this.tasksArr);
+
     this.state = {
-      tasks: tasks,
+      tasks: this.tasksArr,
       value: ''
     };
-
-    this.task = [];
 
     this.handleValueChange = this.handleValueChange.bind(this);
     this.handleBtnSubmit = this.handleBtnSubmit.bind(this);
@@ -24,37 +27,46 @@ export default class TasksForm extends Component {
     });
   }
 
-  handleBtnSubmit(event) {
-
-    event.preventDefault();
-
-    if (!this.state.value.length) {
-      return;
-    }
-
-    this.task.push();
-    console.log(this.task)
+  handleBtnSubmit(values) {
+    const task = new Task(values.name, 'NEW');
+    this.tasksArr = tasks.allTasks.push(task);
+    console.log(this.tasksArr);
     this.setState({
-        tasks: this.state.tasks.allTasks.push(new Task(this.state.value, 'NEW')),
-        value: ''
-      });
-
-    console.log(this.state);
+      tasks: this.tasksArr,
+      value: values.name
+    });
   }
 
   render() {
 
     return (
-        <form onSubmit={this.handleBtnSubmit}>
-          <label>
-            Task name: {this.state.value}
-            <input
-              type="text" className="task-input" value={this.state.value} onChange={this.handleValueChange}/>
-          </label>
-          <input
-            type="submit" value="Add task"/>
-        </form>
+      <LocalForm
+        onSubmit={(values) => this.handleBtnSubmit(values)}
+      >
+        <label>
+          <span>Task name: {this.state.value}</span>
+          <br/>
+          <Control.text
+            className="task-input"
+            model=".name"
+            value={this.state.value}
+            validators={{
+              required: (val) => val && !!val.length
+            }}
+            onChange={this.handleValueChange}
+            required/>
+          <span className="help-block">
+              <Errors
+                model=".name"
+                messages={{
+                  required: 'Write task name'
+                }}
+              />
+            </span>
+        </label>
+        <input
+          type="submit" value="Add task"/>
+      </LocalForm>
     )
   }
-
 }
