4aa4863e235d9b3238d9eb3f3eacdbea032b6542
Marina <marina.todorishina@gmail.com>
2018-03-16 17:21:24 +0200

    Fix tasks handler ability to display & to save tasks in array

--- src/client/components/task/tasks.js
+++ src/client/components/task/tasks.js
@@ -1,24 +1,41 @@
 import Task from "./Task";
+import {statusFilters} from '../../actions/statuses';
 
-const tasks = [];
+let allTasks = [];
+let processingTasks = [];
+let completedTasks = [];
+
+const tasks = {
+  allTasks: allTasks,
+  processingTasks: processingTasks,
+  completedTasks: completedTasks
+};
 
 let tasksItems = [{
   name: 'Ride on a bike',
-  status: 'new'
+  status: 'NEW'
 },
   {
     name: 'Swim',
-    status: 'wait'
+    status: 'IN_WORK'
   },
   {
     name: 'Write code',
-    status: 'in_work'
+    status: 'COMPLETED'
   }
 ];
 
 for (let i = 0; i < tasksItems.length; i++) {
   let taskObj = new Task(tasksItems[i].name, tasksItems[i].status);
-  tasks.push(taskObj);
+  allTasks.push(taskObj);
 }
+
+allTasks.forEach(function (task, index) {
+  if (task.status === statusFilters.IN_WORK) {
+    processingTasks.push(task);
+  } else if (task.status === statusFilters.COMPLETED) {
+    completedTasks.push(task);
+  }
+});
 
 export default tasks;
