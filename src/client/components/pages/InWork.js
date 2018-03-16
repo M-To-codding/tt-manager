db35d3f9eb55aea845f8e8036196ad7326fab2df
Marina <marina.todorishina@gmail.com>
2018-03-16 17:26:10 +0200

    Refactor page inWork, show only tasks with status in work

--- src/client/components/pages/InWork.js
+++ src/client/components/pages/InWork.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import tasks from './../task/tasks';
+import TasksList from './../task/TasksList';
 
 export default class InWork extends Component {
 
-  render() {
-    return <div>This is in work</div>
-  }
-
+    render() {
+        return (
+            <article className="tasks-content">
+                <TasksList {...[tasks.processingTasks]} />
+            </article>
+        )
+    }
 }
