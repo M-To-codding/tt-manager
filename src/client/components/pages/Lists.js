ccefd2c8091bec1ee8c486fc3e757ac6f98e8b8c
Marina <marina.todorishina@gmail.com>
2018-03-16 17:26:46 +0200

    Refactor page Lists, show only completed tasks

--- src/client/components/pages/Lists.js
+++ src/client/components/pages/Lists.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import tasks from './../task/tasks';
+import TasksList from './../task/TasksList';
 
 export default class Lists extends Component {
 
-  render() {
-    return <div>This is lists</div>
-  }
-
+    render() {
+        return (
+            <article className="tasks-content">
+                <TasksList {...[tasks.completedTasks]} />
+            </article>
+        )
+    }
 }
