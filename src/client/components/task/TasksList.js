a53e3d9ed7f3bf4ca0881b5574fd73fdf4ece384
Marina <marina.todorishina@gmail.com>
2018-03-17 00:04:35 +0200

    Remove console log

--- src/client/components/task/TasksList.js
+++ src/client/components/task/TasksList.js
@@ -4,38 +4,36 @@ import {statusFilters} from '../../actions/statuses';
 
 export default class TasksList extends Component {
 
-    constructor(props) {
-        super(props);
-        this.statuses = [];
-        this.tasks = props["0"];
-        console.log(this.tasks);
-        console.log(tasks.allTasks);
+  constructor(props) {
+    super(props);
+    this.statuses = [];
+    this.tasks = props["0"];
 
-        for (let item in statusFilters) {
-            this.statuses.push(item);
-        }
+    for (let item in statusFilters) {
+      this.statuses.push(item);
     }
+  }
 
-    render() {
+  render() {
 
-        const options = this.statuses.map((option, index) =>
-            <option value={option} key={index}>
-                {option}
-            </option>
-        )
+    const options = this.statuses.map((option, index) =>
+      <option value={option} key={index}>
+        {option}
+      </option>
+    )
 
-        const listItems = this.tasks.map((task, index) =>
-            <li key={index} id={index}>
-                <span>{task.name}</span>
-                <select value={task.status}>
-                    {options}
-                </select>
-            </li>
-        )
+    const listItems = this.tasks.map((task, index) =>
+      <li key={index} id={index}>
+        <span>{task.name}</span>
+        <select value={task.status}>
+          {options}
+        </select>
+      </li>
+    )
 
-        return (
-            <ul className="tasks-list">{listItems}</ul>
-        )
-    }
+    return (
+      <ul className="tasks-list">{listItems}</ul>
+    )
+  }
 
 }
