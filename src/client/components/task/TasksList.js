4dc40ea01d9931ab7f58dfd9cf02deec30246044
Marina <marina.todorishina@gmail.com>
2018-03-16 17:23:51 +0200

    Create common tasks list handler

---
+++ src/client/components/task/TasksList.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import tasks from './../task/tasks';
+import {statusFilters} from '../../actions/statuses';
+
+export default class TasksList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.statuses = [];
+        this.tasks = props["0"];
+        console.log(this.tasks);
+        console.log(tasks.allTasks);
+
+        for (let item in statusFilters) {
+            this.statuses.push(item);
+        }
+    }
+
+    render() {
+
+        const options = this.statuses.map((option, index) =>
+            <option value={option} key={index}>
+                {option}
+            </option>
+        )
+
+        const listItems = this.tasks.map((task, index) =>
+            <li key={index} id={index}>
+                <span>{task.name}</span>
+                <select value={task.status}>
+                    {options}
+                </select>
+            </li>
+        )
+
+        return (
+            <ul className="tasks-list">{listItems}</ul>
+        )
+    }
+
+}
