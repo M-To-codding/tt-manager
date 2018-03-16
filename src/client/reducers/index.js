a83ed0777ecccd2106d4c928310f030d2eaf3a64
Marina <marina.todorishina@gmail.com>
2018-03-16 23:58:20 +0200

    Add react-redux-form reducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,9 +1,18 @@
 import { combineReducers } from 'redux';
 import tasks from './tasks';
+import { combineForms } from 'react-redux-form';
+
+const initialTask = {
+    name: '',
+    status: 'NEW',
+};
 
 
 const tasksManager = combineReducers({
-  tasks
+    tasks: tasks,
+    deep: combineForms({
+        task: initialTask,
+    })
 });
 
 export default tasksManager;
