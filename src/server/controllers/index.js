4b9cb058522208ba8ad95ad380706f3a9b2dca8e
Marina <marina.todorishina@gmail.com>
2018-03-17 02:02:45 +0200

    Create main controller

---
+++ src/server/controllers/index.js
@@ -0,0 +1,12 @@
+let expess = require('express'),
+  router = expess.Router(),
+  path = require('path'),
+  client = __dirname + '../../../build/';
+
+router.get('/', function (req, res) {
+  res.setHeader('conten-type', 'application/javascript');
+  res.sendFile(path.join(client + 'index.html'));
+})
+
+
+module.exports = router;
