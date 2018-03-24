60c2aab4f0e197aaa292e31685ca1175983035de
im <marina.todorishina@mail.com>
2018-03-24 03:21:15 +0200

    Get tasks with status 'in work'

--- src/client/components/pages/InWork.js
+++ src/client/components/pages/InWork.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
-import tasks from './../task/tasks';
 import TasksList from './../task/TasksList';
 
 export default class InWork extends Component {
 
+  constructor(props) {
+    super(props);
+    this.routeName = 'inWork';
+  }
+
     render() {
         return (
             <article className="tasks-content">
-                <TasksList {...[tasks.processingTasks]} />
+                <TasksList {...[this.routeName]} />
             </article>
         )
     }
