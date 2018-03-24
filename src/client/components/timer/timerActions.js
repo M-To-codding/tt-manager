cedac2991b67f15f7df1731363bc9ff5ab899fb8
im <marina.todorishina@mail.com>
2018-03-24 05:33:13 +0200

    Add actions for timer

---
+++ src/client/components/timer/timerActions.js
@@ -0,0 +1,27 @@
+export default function setTime(e) {
+
+  let target = e.target,
+    div = document.createElement('div'),
+    resetBtn = '';
+
+  let timer = new Timer(target);
+
+  e.target.setAttribute('disabled', 'true');
+
+  resetBtn = '<button> Reset timer' +
+    '</button>';
+  div.innerHTML = resetBtn;
+  e.target.parentNode.appendChild(div);
+
+}
+
+function Timer(e) {
+  let i = 0;
+  let timer = setInterval(function () {
+    e.textContent = i;
+    parseInt(i++);
+  }, 1000);
+
+}
+
+
