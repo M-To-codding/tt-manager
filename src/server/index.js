3e9f03e0ff0cfbf8e402bbdadea0c40d02c42f67
im <marina.todorishina@mail.com>
2018-03-24 03:24:00 +0200

    Add paths to new controllers

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,9 @@ let express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   index = require('./controllers/index.js'),
-  main = require('./controllers/main.js');
+  main = require('./controllers/main.js'),
+  work = require('./controllers/work.js'),
+  lists = require('./controllers/lists.js');
 
 require('./db');
 
@@ -16,6 +18,8 @@ app.use(express.static(__dirname + './../../build/'));
 
 app.use('/api/v1', index);
 app.use('/api/v1', main);
+app.use('/api/v1', work);
+app.use('/api/v1', lists);
 
 
 module.exports = app;
