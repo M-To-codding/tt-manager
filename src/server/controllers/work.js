87cb4dce218ff029de17df03f34626e77c8d46de
im <marina.todorishina@mail.com>
2018-03-24 03:23:14 +0200

    Create controller for page with tasks in work

---
+++ src/server/controllers/work.js
@@ -0,0 +1,38 @@
+let express = require('express'),
+  router = express.Router(),
+  Task = require('../models/task');
+
+router.get('/inWork', function (req, res) {
+  Task.find({status: 'IN_WORK'}, function (err, tasks) {
+    res.json({tasks});
+    console.log('Server: main controller get: ');
+    console.log(tasks);
+  })
+})
+
+
+router.put('/inWork/:id', function (req, res) {
+  if(req.body.status) {
+    Task.update({_id: req.params.id}, {status: req.body.status}, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(req.body);
+      }
+    })
+  } else {
+    Task.update({_id: req.params.id}, {name: req.body.name}, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(req.body);
+        Task.find({}, function (err, tasks) {
+          res.json({tasks});
+        });
+      }
+    })
+  }
+})
+
+
+module.exports = router;
