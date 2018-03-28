eae46140fe0ad538c08f698661c67bb1678deeab
im <marina.todorishina@mail.com>
2018-03-28 12:36:45 +0300

    Fix server

--- src/server/index.js
+++ src/server/index.js
@@ -14,8 +14,7 @@ app.listen(process.env.PORT || 8080, () => {
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + './../../build/static/'));
-app.use(express.static(__dirname + './../../build/'));
+app.use(express.static('build'));
 
 app.use('/api/v1', index);
 app.use('/api/v1', main);
