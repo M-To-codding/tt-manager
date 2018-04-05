a11c7c56e2ddcac5e37696d585620b15b6d96f83
im <marina.todorishina@mail.com>
2018-04-06 00:48:05 +0300

    Call method for save description. Add res.send for other controllers

--- src/server/controllers/work.js
+++ src/server/controllers/work.js
@@ -7,22 +7,31 @@ router.get('/inWork', function (req, res) {
     res.json({tasks});
     console.log('Server: main controller get: ');
     console.log(tasks);
+
+    if (err) {
+      res.send(err.status);
+    } else {
+      res.send('ok', 200);
+    }
   })
 })
 
 router.put('/inWork/:id', function (req, res) {
-  if(req.body.status) {
+  if (req.body.status) {
     Task.update({_id: req.params.id}, {status: req.body.status}, function (err) {
       if (err) {
         console.log(err);
+        res.send(err.status);
       } else {
         console.log(req.body);
+        res.send('ok', 200);
       }
     })
   } else if (req.body.name) {
     Task.update({_id: req.params.id}, {name: req.body.name}, function (err) {
       if (err) {
         console.log(err);
+        res.send(err.status);
       } else {
         console.log(req.body);
         Task.find({status: 'IN_WORK'}, function (err, tasks) {
@@ -30,20 +39,34 @@ router.put('/inWork/:id', function (req, res) {
         });
       }
     })
-  } else if (req.body.progressTime){
+  } else if (req.body.progressTime) {
     Task.update({_id: req.params.id}, {progressTime: req.body.progressTime}, function (err) {
       if (err) {
         console.log(err);
+        res.send(err.status);
       } else {
         console.log(req.body);
       }
     })
-  } else {
+  } else if (req.body.estimatedTime) {
     Task.update({_id: req.params.id}, {estimatedTime: req.body.estimatedTime}, function (err) {
       if (err) {
         console.log(err);
+        res.end();
       } else {
         console.log(req.body);
+      }
+    })
+  } else if (req.body.description) {
+    Task.update({_id: req.params.id}, {description: req.body.description}, function (err) {
+      if (err) {
+        console.log(err);
+        res.end();
+      } else {
+        console.log(req.body);
+        Task.find({}, function (err, tasks) {
+          res.json({tasks});
+        });
       }
     })
   }
